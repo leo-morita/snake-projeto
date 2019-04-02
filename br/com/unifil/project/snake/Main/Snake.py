56afe09a0d3911e69f66cff6d359b2864144d9ec
Leonardo Morita <leonardomorita1@edu.unifil.br>
2019-04-02 13:36:53 -0300

    Atualizando

--- br/com/unifil/project/snake/Main/Snake.py
+++ br/com/unifil/project/snake/Main/Snake.py
@@ -11,8 +11,10 @@ def main():
     pygame.display.set_icon(logo)
     pygame.display.set_caption("minimal program")
 
-    # Crie uma superfície na tela que tenha o tamanho de 240 x 180
-    screen = pygame.display.set_mode((500,500))
+    # Crie uma superfície na tela que tenha o tamanho de 500 x 500
+    screen_width = 500
+    screen_height = 500
+    screen = pygame.display.set_mode((screen_width,screen_height))
 
     # Defina uma variável para controlar o loop principal
     running = True
@@ -21,19 +23,41 @@ def main():
     while running:
         # Manipulação de eventos, obtém todos os eventos da fila de eventos
         for event in pygame.event.get():
+            # Define a posição da imagem
+            x_pos = 50
+            y_pos = 50
+
+            # Quantos pixels nós movemos o nosso quadro
+            step_x = 10
+            step_y = 10
+
+            # Verifique se a imagem ainda está na tela, se não, mudar de direção
+            if x_pos > screen_width-64 or x_pos < 0:
+                step_x = -step_x
+            if y_pos > screen_height-64 or y_pos < 0:
+                step_y = -step_y
+
+            # Atualizar a posição da imagem
+            x_pos += step_x # Mover para a direita
+            y_pos += step_y # Mover para baixo
+
             # Carregar imagem
             image = pygame.image.load("imagem01.jpg")
             background = pygame.image.load("background.jpg")
-
-
 
             # Com esta função, faz com que o background do cenário fique tudo branco
             #screen.fill((255, 255, 255))
 
             # blit - O que faz é copiar os pixels da superfície da imagem para a superfície da tela
             screen.blit(background, (0, 0))
+
+            # Faz com que a imagem fique transparente
             image.set_alpha(128)
+
+            # Esta função, funciona em algumas imagens, caso esta imagem não tem um valor alpha, aí faz com que tire as
+            # bordas da imagem selecionada
             #image.set_colorkey((255, 0, 255))
+
             screen.blit(image, (50, 50))
             pygame.display.flip()
 
